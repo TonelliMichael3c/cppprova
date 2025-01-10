ee4e63400efd2972001365931d7475570d285dcb
TonelliMichael <115994356+TonelliMichael3c@users.noreply.github.com>
2025-01-10 10:33:47 +0000

    aggiunto esercizio e corretto errore

---
+++ es3.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>;
+using namespace std;
+int main()
+{
+    int matrice[2][2];
+    int r;
+    int c;
+   
+    for (r=0;r<2;r++){
+        for (c=0;c<2;c++){
+
+
+cout << "inserisci il numero" << endl;
+
+
+cin >> matrice[r][c];
+
+
+if (matrice[r][c]==3){
+    cout << "il numero 3 è presente"<< endl; }
+
+
+else {
+    cout << "il numero non è presente" << endl;
+}
+
+
+}
+        }
+    }
